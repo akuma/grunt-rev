a4a906ebc814e63cd05776e9971de2bd36a6539a
Akuma Huang <ihuangwj@gmail.com>
2013-06-19 18:02:00 +0800

    Add rename option (default false) and change filename to [name].[hash].[ext]

--- tasks/rev.js
+++ tasks/rev.js
@@ -21,24 +21,39 @@ module.exports = function(grunt) {
     return hash.digest(encoding);
   }
 
+  function getNameAndExtension(filename) {
+    var i = filename.lastIndexOf('.'),
+        name = (i < 0) ? filename : filename.substr(0, i),
+        ext = (i < 0) ? '' : filename.substr(i + 1);
+    return {name: name, ext: ext};
+  }
+
   grunt.registerMultiTask('rev', 'Prefix static asset file names with a content hash', function() {
 
     var options = this.options({
       encoding: 'utf8',
       algorithm: 'md5',
-      length: 8
+      length: 8,
+      rename: false
     });
 
     this.files.forEach(function(filePair) {
       filePair.src.forEach(function(f) {
 
         var hash = md5(f, options.algorithm, 'hex', options.encoding),
-          prefix = hash.slice(0, options.length),
-          renamed = [prefix, path.basename(f)].join('.'),
-          outPath = path.resolve(path.dirname(f), renamed);
+            prefix = hash.slice(0, options.length),
+            filename = path.basename(f),
+            nameAndExt = getNameAndExtension(filename),
+            renamed = [nameAndExt.name, prefix, nameAndExt.ext].join('.'),
+            outPath = path.resolve(path.dirname(f), renamed);
 
         grunt.verbose.ok().ok(hash);
-        fs.renameSync(f, outPath);
+
+        if (options.rename) {
+          fs.renameSync(f, outPath);
+        } else {
+          fs.createReadStream(f).pipe(fs.createWriteStream(outPath));
+        }
         grunt.log.write(f + ' ').ok(renamed);
 
       });
